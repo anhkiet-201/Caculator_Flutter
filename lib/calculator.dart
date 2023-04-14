0ef581bcc2d930170b0b11ec9cc8662f3799923e
Ngọc Trinh <igg.anhkiet1@gmail.com>
2023-04-14 10:16:24 +0700

    add animation

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -10,14 +10,24 @@ class Calculator extends StatefulWidget {
   State<Calculator> createState() => _CalculatorState();
 }
 
-class _CalculatorState extends State<Calculator> {
+class _CalculatorState extends State<Calculator> with SingleTickerProviderStateMixin {
 
+  late Animation<double> animation;
+  late AnimationController animationController;
+  late Tween<double> tween;
   final ScrollController _controller = ScrollController();
-  final _operator = ['+','-','×','÷'];
-  List<String> _input = [];
-  List<String> _expression = [];
+  final List<String> _input = [];
+  final List<String> _expression = [];
   String _currentInput = '';
   String _result = '';
+
+  @override
+  void initState() {
+    animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 200));
+    tween = Tween<double>(begin: -25, end: 25);
+    animation = tween.animate(CurvedAnimation(parent: animationController, curve: Curves.easeInOut));
+    super.initState();
+  }
 
   _scrollToEnd(){
     if(_controller.offset>0){
@@ -25,10 +35,8 @@ class _CalculatorState extends State<Calculator> {
     }
   }
 
-
   _handleExpression(){
     List<String> t = List.from(_expression);
-    print(t);
     if(_currentInput.isEmpty && t.isEmpty) {
       _result = '';
       return;
@@ -43,19 +51,30 @@ class _CalculatorState extends State<Calculator> {
         return;
       }
     }
-    _result = calculatePostFix(infixTopostfix(t)).toString();
+    _result = calculatePostFix(infixTopostfix(t)).toStringAsFixed(2);
   }
 
   void buttonOnClick(String value){
-    setState(() {
+    animationController.reverse();
+    if(value == '.'){
+      if(!_currentInput.contains(value)){
+        if(!isANum(_currentInput)){
+          _currentInput += '0.';
+        }else{
+          _currentInput += value;
+        }
+      }
+    }else{
       _currentInput += value;
+    }
+    setState(() {
       _handleExpression();
     });
     _scrollToEnd();
   }
 
-
   void operatorOnClick(String value){
+    animationController.reverse();
     if(value == 'C'){
       if(_currentInput.isNotEmpty){
         _currentInput = _currentInput.substring(0,_currentInput.length-1);
@@ -108,6 +127,14 @@ class _CalculatorState extends State<Calculator> {
     _scrollToEnd();
   }
 
+  _handleAC()=>setState(
+      (){
+        _input.clear();
+        _expression.clear();
+        _currentInput = '';
+      }
+  );
+
   List<TextSpan> _colorText(List<String> text){
     List<String> tmp = List.from(text);
     if(tmp.isEmpty && _currentInput.isEmpty){
@@ -122,7 +149,7 @@ class _CalculatorState extends State<Calculator> {
     tmp.last += _currentInput;
 
     return tmp.map((e){
-      if(_operator.contains(e)) {
+      if(!isANum(e)) {
         return TextSpan(
           text: ' $e ',
           style: const TextStyle(
@@ -156,52 +183,62 @@ class _CalculatorState extends State<Calculator> {
                           blurRadius: 10),
                     ],
                     borderRadius: BorderRadius.all(Radius.circular(5))),
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: SingleChildScrollView(
-                            physics: const BouncingScrollPhysics(),
-                            scrollDirection: Axis.horizontal,
-                            child: SelectableText(
-                              _result.isEmpty ? '0' : _result,
-                              maxLines: 1,
-                              style: const TextStyle(
-                                  fontSize: 60
+                child: AnimatedBuilder(
+                  animation: animation,
+                  builder: (_,c){
+                    double top = 100 + animation.value;
+                    double bot = 100 - animation.value;
+                    return Column(
+                      children: [
+                        Expanded(
+                          flex:top.toInt(),
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: SingleChildScrollView(
+                                physics: const BouncingScrollPhysics(),
+                                scrollDirection: Axis.horizontal,
+                                child: SelectableText(
+                                  _result.isEmpty ? '0' : _result,
+                                  maxLines: 1,
+                                  style: TextStyle(
+                                      fontSize: 60 * (top/100),
+                                    color: Colors.black.withOpacity(0.5 + (top/100 -1))
+                                  ),
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ),
-                    Expanded(
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: SingleChildScrollView(
-                            controller: _controller,
-                            reverse: true,
-                            physics: const BouncingScrollPhysics(),
-                            scrollDirection: Axis.horizontal,
-                            child: SelectableText.rich(
-                              TextSpan(
-                                children: _colorText(_input)
-                              ),
-                              maxLines: 1,
-                              style: const TextStyle(
-                                fontSize: 60
+                        Expanded(
+                          flex: bot.toInt(),
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: SingleChildScrollView(
+                                controller: _controller,
+                                reverse: true,
+                                physics: const BouncingScrollPhysics(),
+                                scrollDirection: Axis.horizontal,
+                                child: SelectableText.rich(
+                                  TextSpan(
+                                      children: _colorText(_input)
+                                  ),
+                                  maxLines: 1,
+                                  style: TextStyle(
+                                    fontSize: 60 * (bot/100),
+                                  ),
+                                )
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ),
-                  ],
-                ),
+                      ],
+                    );
+                  },
+                )
               ),
             ),
             Expanded(
@@ -248,55 +285,63 @@ class _CalculatorState extends State<Calculator> {
                   Row(
                     children: [
                       Expanded(
-                        child: Container(
-                          height: 50,
-                          margin: const EdgeInsets.symmetric(horizontal: 20),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              boxShadow: [
-                                BoxShadow(
-                                    offset: const Offset(0,6),
-                                    blurRadius: 25,
-                                    color: const Color(0xff5293cd).withOpacity(0.5)
+                        child: GestureDetector(
+                          child: Container(
+                            height: 50,
+                            margin: const EdgeInsets.symmetric(horizontal: 20),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                      offset: const Offset(0,6),
+                                      blurRadius: 25,
+                                      color: const Color(0xff5293cd).withOpacity(0.5)
+                                  ),
+                                ],
+                                borderRadius: BorderRadius.circular(5)
+                            ),
+                            child: const Center(
+                              child: Text(
+                                'AC',
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    color: Color(0xfffbafb0)
                                 ),
-                              ],
-                              borderRadius: BorderRadius.circular(5)
-                          ),
-                          child: const Center(
-                            child: Text(
-                              'AC',
-                              style: TextStyle(
-                                  fontSize: 25,
-                                  color: Color(0xfffbafb0)
                               ),
                             ),
                           ),
+                          onTap: ()=>_handleAC(),
                         ),
                       ),
                       Expanded(
-                        child: Container(
-                          height: 50,
-                          margin: const EdgeInsets.symmetric(horizontal: 20),
-                          decoration: BoxDecoration(
-                              color: const Color(0xff57b3fe),
-                              boxShadow: const [
-                                BoxShadow(
-                                    offset: Offset(0,6),
-                                    blurRadius: 25,
-                                    color: Color(0xff5293cd)
+                        child: GestureDetector(
+                          child: Container(
+                            height: 50,
+                            margin: const EdgeInsets.symmetric(horizontal: 20),
+                            decoration: BoxDecoration(
+                                color: const Color(0xff57b3fe),
+                                boxShadow: const [
+                                  BoxShadow(
+                                      offset: Offset(0,6),
+                                      blurRadius: 25,
+                                      color: Color(0xff5293cd)
+                                  ),
+                                ],
+                                borderRadius: BorderRadius.circular(5)
+                            ),
+                            child: const Center(
+                              child: Text(
+                                '=',
+                                style: TextStyle(
+                                    fontSize: 35,
+                                    color: Colors.white
                                 ),
-                              ],
-                              borderRadius: BorderRadius.circular(5)
-                          ),
-                          child: const Center(
-                            child: Text(
-                              '=',
-                              style: TextStyle(
-                                  fontSize: 35,
-                                  color: Colors.white
                               ),
                             ),
                           ),
+                          onTap: (){
+                            animationController.forward();
+                          },
                         ),
                       )
                     ],
